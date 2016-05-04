cc4a2c31955b71fbf8ed09800e828255524cf936
luocl <luochunlei@microtrust.com.cn>
2016-05-05 00:39:44 +0800

    修改了vs的一些问题：已经启动，就不再启动。

--- vs/src/vs.py
+++ vs/src/vs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-"""
-在系统中加入全局按键处理。
-"""
+#
+# 在系统中加入全局按键处理。
+#
 
 import os, sys, gtk, getopt, keybinder, subprocess
 
@@ -18,29 +18,34 @@ def start_vx():
                          cwd = work_dir,
                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
     
-    # 直到命令结束。
-    (stdoutdata, stderrdata) = p.communicate()
-    print "end of process."
-    
-#     obj_result = p.poll()
-#     if obj_result == 0:
-#         print "is OK"
-#     else:
-#         print "is Error"
+    # 没有必要等到命令结束。
+    p.communicate()
+    #print "end of process."
     
 def on_start_pressed():
-    ''' 启动命令。 '''
+    # 启动命令。
     start_vx()
     
 def on_stop_pressed():
-    ''' 关闭自己。'''
+    # 关闭自己。
     gtk.main_quit()
+    
+def find_same_process(name):
+    # return true:found, false:NOT found
+    r = os.popen('ps -ef | grep %s | grep python | grep -v grep' % (name))
+    
+    # 因为此时已经启动vs.py，所以有一个。
+    #for l in r.readlines():
+    #    print l
+    count = len(r.readlines()) 
+    #print "count %d" % (count)
+    return count > 1
 
 def main(argv):
     
     # 分析参数。
     try:
-        opts, args = getopt.getopt(argv[1:], 'hd:', ['dir='])
+        opts, args = getopt.getopt(argv[1:], 'h')
     except getopt.GetoptError, err:
         print str(err)
         usage()
@@ -51,21 +56,22 @@ def main(argv):
             '显示帮助信息'
             usage()
             sys.exit(0)
-        elif o in ('-d', '--dir'):
-            # 目前没有用。
-            workshop_dir = a
         else:
             print 'unknown arguments.'
             usage()
             sys.exit(2)
+    # 如果发现已经存在这个vs，就不再启动
+    if find_same_process("vs.py"):
+        print "The vs.py was executing."
+        return
     
-    # 开始连接快捷键
+    # 连接快捷键
     keybinder1 = keybinder.bind("F12",  on_start_pressed)
-    keybinder2 = keybinder.bind("<Control>F12",  on_stop_pressed)
+    keybinder2 = keybinder.bind("<control>F12",  on_stop_pressed)
     
     gtk.main()
 
 if __name__ == '__main__':
-    '''  主入口  '''
+    #  主入口  
     main(sys.argv)
     
