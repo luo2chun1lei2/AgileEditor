9343fcbda12eb77e2b56ee0a3ff2faa7702f0581
luocl <luochunlei@microtrust.com.cn>
2016-05-09 20:46:23 +0800

    修改注释。

--- ve/src/ViewHelp.py
+++ ve/src/ViewHelp.py
@@ -1,21 +1,20 @@
 #-*- coding:utf-8 -*-
 
-'''
-对与Ide有帮助和支持的功能。
-'''
+# 对与Ide有帮助和支持的功能。
+# TODO 目前比较简陋，以后还需要加入版本升级，和帮助功能。
 
 from gi.repository import Gtk, Gdk
 
 class ViewDialogInfo(Gtk.Dialog):
-    ''' 显示“关于”信息的对话框 '''
+    # 显示“关于”信息的对话框
+    
     def __init__(self, parent):
         Gtk.Dialog.__init__(self, "信息", parent, 0,
                             (Gtk.STOCK_OK, Gtk.ResponseType.OK))
         
         self.set_default_size(300, 200)
         
-        info = "Workshop的集成开发环境，版本0.0.1。\n" + \
-            "目前提供项目管理、代码编辑。"
+        info = "Workshop的集成开发环境，版本0.0.2。\n目前提供项目管理、代码编辑。"
         label= Gtk.Label(info)
         
         box = self.get_content_area()
--- ve/src/ViewTagList.py
+++ ve/src/ViewTagList.py
@@ -1,17 +1,15 @@
 #-*- coding:utf-8 -*-
 
-'''
-在List中显示Tags。
-'''
+# 在List中显示Tags。
+# 显示信息不够完成，还缺少idutils工具支持。
 
+import logging
 from gi.repository import Gtk, GObject, GLib
 from ModelTags import ModelTag
 
 class ViewTagList:
-    '''
-    定制一个ListView，内部显示Tag。
-    ideWindow:EditorWindow:总的编辑窗口。
-    '''
+    # 定制一个ListView，内部显示Tag。
+    # ideWindow:EditorWindow:总的编辑窗口。
     
     # 设定一个栏目的枚举常量。
     (
@@ -20,13 +18,15 @@ class ViewTagList:
      NUM_COLUMNS) = range(3)
 
     def __init__(self, ideWindow):
+        # ideWindow:ViewWindow:主画面
 
         self.ideWindow = ideWindow
 
         #vbox = Gtk.VBox(spacing=8)
         #self.window.add(vbox)
 
-        #label = Gtk.Label(label='This is the bug list (note: not based on real data, it would be nice to have a nice ODBC interface to bugzilla or so, though).')
+        #label = Gtk.Label(label='This is the bug list 
+        # (note: not based on real data, it would be nice to have a nice ODBC interface to bugzilla or so, though).')
         #vbox.pack_start(label, False, False, 0)
 
         sw = Gtk.ScrolledWindow()
@@ -48,7 +48,7 @@ class ViewTagList:
         self.taglistview = treeview
         
     def create_model(self, tags):
-        ''' 根据Tag生成对应的Model。 '''
+        # 根据Tag生成对应的Model。
         model = Gtk.ListStore(GObject.TYPE_INT, str)
 
         for tag in tags:
@@ -57,7 +57,7 @@ class ViewTagList:
         return model
         
     def set_model(self, tags):
-        ''' 设置模型 '''
+        # 设置模型
         self.model = self.create_model(tags) 
         self.taglistview.set_model(self.model)
 
@@ -81,12 +81,14 @@ class ViewTagList:
         treeview.append_column(column)
         
     def on_row_activated(self, treeview, path, column):
-        ''' 当双击了Tag时 '''
+        # 当双击了Tag时
+        
+        # 得到行号
         model = treeview.get_model()
         miter = model.get_iter(path)
-        line_no = model.get_value(miter, 0)    # 得到行号
+        line_no = model.get_value(miter, 0)
         
-        #print 'tag line no=', line_no
+        logging.debug('tag line no=%d' % line_no)
         # 跳转到对应的行。
         self.ideWindow.ide_goto_line(line_no)
         
